3f6076eaa552598be9c5e20e300017e216f87930
Timo Galuschka <timo.galuschka@hs-kempten.de>
2023-05-23 11:32:09 +0200

    uses seperate model for main image and transition

--- src/outpainting.py
+++ src/outpainting.py
@@ -1,6 +1,6 @@
 import PIL.Image
 import torch
-from diffusers import StableDiffusionInpaintPipeline
+from diffusers import StableDiffusionInpaintPipeline, StableDiffusionPipeline
 
 from src.aesthetic_predictor import AestheticPredictor
 from src.create_prompt import create_prompt_from_news
@@ -17,14 +17,24 @@ class Outpainting:
             self.outpainting_config = OutPaintingConfig()
             self.aesthetic_predictor = AestheticPredictor()
 
-            # init model/pipe
-            model = self.outpainting_config.get_config("outpainting", "model") or "runwayml/stable-diffusion-inpainting"
-            self.pipe = StableDiffusionInpaintPipeline.from_pretrained(
-                model,
+            # init inpainting model/pipe
+            inpainting_model = self.outpainting_config.get_config("outpainting", "inpainting_model") or "runwayml/stable-diffusion-inpainting"
+            self.inpainting_pipe = StableDiffusionInpaintPipeline.from_pretrained(
+                inpainting_model,
                 revision="fp16",
                 torch_dtype=torch.float16
             )
-            self.pipe.to("cuda")
+            self.inpainting_pipe.to("cuda")
+
+            # init main model/pipe
+            main_model = self.outpainting_config.get_config("outpainting", "main_model") or "runwayml/stable-diffusion-v1-5"
+            self.main_pipe = StableDiffusionPipeline.from_pretrained(
+                main_model,
+                revision="fp16",
+                torch_dtype=torch.float16
+            )
+            self.main_pipe.to("cuda")
+
 
     def generate_image(self):
         # load params
@@ -32,58 +42,87 @@ class Outpainting:
         prompt = create_prompt_from_news() + ', ' + default_prompt
         negative_prompt = self.outpainting_config.get_config("outpainting", "negative_prompt")
         guidance_scale = self.outpainting_config.get_config_float("outpainting", "guidance_scale") or 7.5
+        guidance_scale_trans = self.outpainting_config.get_config_float("outpainting", "guidance_scale_trans") or 7.5
         num_inference_steps = self.outpainting_config.get_config_int("outpainting", "num_inference_steps") or 10
         height = self.outpainting_config.get_config_int("outpainting", "height") or 512
         width = self.outpainting_config.get_config_int("outpainting", "width") or 512
 
-        # load image
-        init_image = read_image_batched("outpainting", "image")
-        first_image = False
-        if init_image is None:
-            init_image = PIL.Image.new("RGB", (width, height), (0, 0, 0))
-            first_image = True
-
-        # set params
-        output_height = init_image.height
-        output_width = init_image.width + (width if not first_image else 0)
-
-        # create working image
-        working_image = PIL.Image.new(init_image.mode,
-                                      (init_image.width + (width if not first_image else 0), init_image.height),
-                                      (0, 0, 0))
-        working_image.paste(init_image, (0, 0, init_image.width, init_image.height))
-
-        # create mask image
-        mask_image = PIL.Image.new("RGB", (init_image.width + (width if not first_image else 0), init_image.height),
-                                   (0, 0, 0))
-        mask_image.paste((255, 255, 255), (
-            init_image.width if not first_image else 0, 0, init_image.width + (width if not first_image else 0),
-            init_image.height))
-
+        # generate main image
         quality = 0
         quality_threshold = self.outpainting_config.get_config_float("outpainting", "quality_threshold")
         quality_step = self.outpainting_config.get_config_float("outpainting", "quality_step")
+        generated_image = PIL.Image.new("RGB", (width, height), (0, 0, 0))
         while quality < quality_threshold:
             # generate image
-            generated_image = self.pipe(
+            generated_image = self.main_pipe(
                 prompt=prompt,
                 negative_prompt=negative_prompt,
-                image=working_image,
-                mask_image=mask_image,
                 guidance_scale=guidance_scale,
                 num_inference_steps=num_inference_steps,
-                height=output_height,
-                width=output_width,
+                height=height,
+                width=width,
             ).images[0]
 
             # check quality
             quality = self.aesthetic_predictor.eval_image(generated_image)
             quality_threshold -= quality_step
-        else:
-            if not first_image:
-                cropped_image = generated_image.crop((width, 0, width * 2, height))
+
+        # generate transition
+        init_image = read_image_batched("outpainting", "image")
+        if init_image is not None:
+            # load image
+            first_image = False
+            if init_image is None:
+                init_image = PIL.Image.new("RGB", (width, height), (0, 0, 0))
+                first_image = True
+
+            # set params
+            output_height = init_image.height
+            output_width = init_image.width + (2 * width if not first_image else 0)
+
+            # create working image
+            working_image = PIL.Image.new(init_image.mode,
+                                        (init_image.width + (2 * width if not first_image else 0), init_image.height),
+                                        (0, 0, 0))
+            working_image.paste(init_image, (0, 0, init_image.width, init_image.height))    # left image
+            working_image.paste(generated_image, (init_image.width + width, 0, init_image.width + width + generated_image.width, generated_image.height))    # right image
+
+            # create mask image
+            mask_image = PIL.Image.new("RGB", (init_image.width + (2 * width if not first_image else 0), init_image.height),
+                                    (0, 0, 0))
+            mask_image.paste((255, 255, 255), (
+                init_image.width if not first_image else 0, 0, init_image.width + (width if not first_image else 0),
+                init_image.height))
+
+            quality = 0
+            quality_threshold = self.outpainting_config.get_config_float("outpainting", "quality_threshold")
+            quality_step = self.outpainting_config.get_config_float("outpainting", "quality_step")
+            generated_transition = PIL.Image.new("RGB", (width, height), (0, 0, 0))
+            while quality < quality_threshold:
+                # generate image
+                generated_transition = self.inpainting_pipe(
+                    prompt=default_prompt,
+                    negative_prompt=negative_prompt,
+                    image=working_image,
+                    mask_image=mask_image,
+                    guidance_scale=guidance_scale_trans,
+                    num_inference_steps=num_inference_steps,
+                    height=output_height,
+                    width=output_width,
+                ).images[0]
+
+                # check quality
+                quality = self.aesthetic_predictor.eval_image(generated_transition)
+                quality_threshold -= quality_step
+            else:
+                cropped_image = generated_transition.crop((width, 0, width * 2, height))
                 filename = save_image_batched(cropped_image, "outpainting", "image")
                 convert_img_to_webp(f"outpainting/{filename}")
-            else:
-                filename = save_image(generated_image, "outpainting", "0001_image")
-                convert_img_to_webp(f"outpainting/{filename}")
+
+        # save main image
+        if init_image is not None:
+            filename = save_image_batched(generated_image, "outpainting", "image")
+            convert_img_to_webp(f"outpainting/{filename}")
+        else:
+            filename = save_image(generated_image, "outpainting", "0001_image")
+            convert_img_to_webp(f"outpainting/{filename}")
